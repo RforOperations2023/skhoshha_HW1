20e7db31043132d13cfd5539af61d92196da52e8
skhoshhal7 <122052201+skhoshhal7@users.noreply.github.com>
2023-02-03 17:52:14 -0500

    First Draft:
    Includes a scatterplot, bar graph, and table of the data

---
+++ spotify/app.R
@@ -0,0 +1,144 @@
+library(shiny)
+library(ggplot2)
+library(DT)
+library(stringr)
+library(dplyr)
+library(tools)
+data = read.csv("top50spotify.csv")
+
+
+# Define UI for application that plots features of top 50 spotify songs --------
+ui <- fluidPage(
+  
+  shinythemes::themeSelector(),
+  
+  theme = shinythemes::shinytheme("united"),
+
+    # Application title
+    titlePanel("Top 50 Songs on Spotify from 2010-2019"),
+
+    # Sidebar 
+    sidebarLayout(
+        sidebarPanel(
+          
+          selectInput(inputId = "x", 
+                      label = "X-axis:",
+                      choices = c("duration",
+                                  "bpm",
+                                  "energy"), 
+                      selected = "duration"),
+          
+          selectInput(inputId = "y", 
+                      label = "Y-axis:",
+                      choices = c("popular"), 
+                      selected = "popular"),
+          
+          selectInput(inputId = "z", 
+                      label = "Color by:",
+                      choices = "genre",
+                      selected = "genre"),
+          
+          checkboxGroupInput(inputId = "selected_type",
+                             label = "Select Song Genre",
+                             choices = c("dance pop", "pop", "hip hop", "big room"
+                                         , "latin", "canadian pop", 
+                                         "barbadian pop", "boy band", "electropop"),
+                             selected = c("dance pop", "pop", "hip hop", 
+                                          "big room"
+                                          , "latin", "canadian pop", 
+                                          "barbadian pop", "boy band", 
+                                          "electropop")),
+          
+          hr(),
+          
+          selectInput(inputId = "x1", 
+                      label = "X-axis:",
+                      choices = c("loudness",
+                                  "liveness",
+                                  "dance", 
+                                  "valence", 
+                                  "acoustics",
+                                  "speechiness"), 
+                      selected = "duration"),
+          
+          selectInput(inputId = "y1", 
+                      label = "Y-axis:",
+                      choices = c("popular"), 
+                      selected = "popular"),
+          
+          hr(),
+          
+          checkboxInput(inputId = "show_data",
+                        label = "Show data table",
+                        value = TRUE)
+        
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+          plotOutput(outputId = "scatterplot"),
+          br(),        # a little bit of visual separation
+          
+          # Print number of obs plotted ---------------------------------
+          uiOutput(outputId = "n"),
+          br(), br(),    # a little bit of visual separation
+          
+          plotOutput(outputId = "bargraph"),
+          
+          br(), br(),
+          
+          # Show data table ---------------------------------------------
+          DT::dataTableOutput(outputId = "data")
+        )
+    )
+)
+
+# Define server 
+server <- function(input, output) {
+  
+  # Create a subset of data filtering for selected title types ------
+  data_subset <- reactive({
+    req(input$selected_type) # ensure availablity of value before proceeding
+    filter(data, genre %in% input$selected_type)
+  })
+  
+  # Create scatterplot object the plotOutput function is expecting --
+  output$scatterplot <- renderPlot({
+    ggplot(data = data_subset(), aes_string(x = input$x, y = input$y,
+                                              color = input$z)) +
+      geom_point() +
+      labs(x = toTitleCase(str_replace_all(input$x, "_", " ")),
+           y = toTitleCase(str_replace_all(input$y, "_", " ")))
+  })
+  
+  output$bargraph <- renderPlot({
+    ggplot(data = data_subset(), aes_string(x = input$x1, y = input$y1
+    )) +
+      geom_bar(stat="identity", fill="steelblue") +
+      labs(x = toTitleCase(str_replace_all(input$x1, "_", " ")),
+           y = toTitleCase(str_replace_all(input$y1, "_", " ")))
+  })
+  
+  # Print number of movies plotted ----------------------------------
+  output$n <- renderUI({
+    types <- data_subset()$genre %>% 
+      factor(levels = input$selected_type) 
+    counts <- table(types)
+    
+    HTML(paste("There are", counts, input$selected_type, "songs in this dataset. <br>"))
+  })
+  
+  # Print data table if checked -------------------------------------
+  output$data <- DT::renderDataTable(
+    if(input$show_data){
+      DT::datatable(data = data[, 1:15], 
+                    options = list(pageLength = 10), 
+                    rownames = FALSE)
+    }
+  )
+}
+
+    
+
+# Run the application 
+shinyApp(ui = ui, server = server)
